ecbc783c673ae0be1b5dda4bc373e6eca1b1fd98
Jennifer <jennifacarey@gmail.com>
2021-03-09 17:29:42 +0000

    content added

--- game-reviews/components/home-banner.js
+++ game-reviews/components/home-banner.js
@@ -1,5 +1,11 @@
+import React from "react";
+
 const HomeBanner = () => {
    return (
-      
+      <div className="banner banner-home">
+         <h1>Safe gaming for kids</h1>
+         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum at nisi.</p>
+         <a href="#" className="btn btn-primary">Start searching!</a>
+      </div>
    )
 }
