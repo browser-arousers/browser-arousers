a3321a9b96ad38d24cac3d3de27e7c728f3b2989
Amelia <amelia.riding@Sofology.co.uk>
2021-03-10 11:42:17 +0000

    Navbar JS and CSS

---
+++ game-reviews/src/components/navbar.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import './navbar.css'
+
+export default function Navbar() {
+    return (
+        <>
+        <nav className = "navbar">
+            <div className ="navContainer">
+                <Link to="/" className="navLogo">
+                    <h3>P</h3>
+                    <h3 className="G1">G</h3>
+                    <p>tips</p>
+                </Link>
+            </div>
+        </nav>
+        </>
+    )
+}
