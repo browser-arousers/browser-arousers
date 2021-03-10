316409d834e49eaad4cd52fe69ea885357a155e3
Jennifer <jennifacarey@gmail.com>
2021-03-10 16:02:48 +0000

    Auto stash before merge of "main" and "origin/main"

--- game-reviews/src/App.js
+++ game-reviews/src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
+<<<<<<< Updated upstream
 import {BrowserRouter as Router} from 'react-router-dom'
 import HomeBanner from "./components/home-banner"
 import Navbar from "./components/navbar"
+=======
+import HomeBanner from "./components/home-banner";
+>>>>>>> Stashed changes
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <Router>
         <Navbar/>
       <HomeBanner />
       </Router>
+=======
+      <HomeBanner />
+>>>>>>> Stashed changes
     </div>
   );
 }
