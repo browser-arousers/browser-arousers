4e5a57e37066c79be755a6b5a38c65bc68275c0c
Jennifer <jennifacarey@gmail.com>
2021-03-10 15:57:39 +0000

    syntax correction

--- game-reviews/src/components/home-banner.js
+++ game-reviews/src/components/home-banner.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomeBanner = () => {
    return (
       <div className="banner banner-home">
-         <div class="banner-home-content">
+         <div className="banner-home-content">
             <h1>Parental Guidance Tips</h1>
             <p>Access hundreds of game reviews, reviewed and vetted by parents like you so your children can play safely.</p>
             <a href="#" className="btn btn-primary">Sign up now</a>
