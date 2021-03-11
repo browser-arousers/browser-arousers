969816a11f2380f67583c4ef6f5b7f6ca9a9d524
Amelia <amelia.riding@Sofology.co.uk>
2021-03-11 16:40:12 +0000

    Amended Signup and  APP

--- game-reviews/src/components/Signup.js
+++ game-reviews/src/components/Signup.js
@@ -1,36 +1,4 @@
 import { useState } from 'react';
-<<<<<<< HEAD
-import Axios from 'axios';
-import './Signup.css';
-import React from 'react';
-
-export default function Signup(props) {
-
-    const [username, setUser] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordCheck, setPasswordCheck] = useState("")
-
-    const sendDetailsToServer = () => {
-        
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(state.password === state.confirmPassword) {
-            sendDetailsToServer()
-        } else {
-            props.showError('Passwords do not match')
-        }
-    }
-
-    return (
-        <div>
-            <form className="form-group">
-
-            <label>Username</label>
-                <input type="username"
-=======
 import axios from 'axios';
 import './Signup.css';
 
@@ -76,20 +44,15 @@ export default function Signup(props) {
                     id="username"
                     onChange={handleSubmit}
                     type="text"
->>>>>>> main
                     className="formInput"
                     placeholder="Enter Username"
                 />
 
                 <label>Email Address</label>
-<<<<<<< HEAD
-                <input type="email"
-=======
                 <input
                     id="Email"
                     onChange={handleSubmit}
                     type="email"
->>>>>>> main
                     className="formInput"
                     placeholder="Enter email"
                 />
@@ -100,14 +63,10 @@ export default function Signup(props) {
 
                 <div className="form-group">
                     <label> Password </label>
-<<<<<<< HEAD
-                    <input type="password"
-=======
                     <input
                         id="password"
                         onChange={handleSubmit}
                         type="password"
->>>>>>> main
                         className="formInput"
                         placeholder="Password">
                     </input>
@@ -115,25 +74,17 @@ export default function Signup(props) {
 
                 <div className="form-group">
                     <label> Confirm Password </label>
-<<<<<<< HEAD
-                    <input type="password"
-=======
                     <input
                         id="passwordCheck"
                         onChange={handleSubmit}
                         type="password"
->>>>>>> main
                         className="formInput"
                         placeholder="Confirm Password">
                     </input>
                 </div>
 
-<<<<<<< HEAD
-                <button type="submit"
-=======
                 <button
                     type="submit"
->>>>>>> main
                     className="submit-btn"
                 >
                     Sign Up
