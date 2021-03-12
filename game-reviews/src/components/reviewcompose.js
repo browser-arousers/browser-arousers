7a22c91b71cf002a1676ffa69896e62ffa82669e
Jim <jadavey@hotmail.com>
2021-03-12 14:28:23 +0000

    Reviews sent but not posting
    almost there

--- game-reviews/src/components/reviewcompose.js
+++ game-reviews/src/components/reviewcompose.js
@@ -1,28 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import axios from "axios";
 import "./reviewcompose.css";
 
 const ReviewCompose = (props) => {
-  const [reviewBody, setReview] = useState("");
+  const [body, setBody] = useState([]);
+  const [title, setTitle] = useState([]);
 
   const sendDetailsToServer = (e) => {
     e.preventDefault();
     const data = {
-      reviewBody: reviewBody,
+      body: body,
+      title: title,
     };
     console.log(data);
     axios.post({
+      method: "POST",
       //url location to be determined:
-      url: "http://localhost:5000/addreview",
+      url: "http://localhost:5000/review/addreview",
       mode: "cors",
       data: data,
     });
   };
 
   const handleSubmit = (event) => {
-    if (event.target.id === "reviewBody") {
-      setReview(event.target.value);
+    if (event.target.id === "body") {
+      setBody(event.target.value)
+    } else if (event.target.id === "title") {
+      setTitle(event.target.value)
     }
   };
 
@@ -32,22 +37,27 @@ const ReviewCompose = (props) => {
         {/* <div className="userPic">
                 <ProfilePicture src={somesourcehere} size="medium" />
             </div> */}
+        <input
+                    id="title"
+                    onChange={handleSubmit}
+                    type="text"
+                    className="formInput"
+                    placeholder="Review Title"
+                />
+        
         <div className="inputText">
           <TextareaAutosize
             className="inputTextArea"
-            id="reviewBody"
+            id="body"
             placeholder="Add Your Review Here!"
             maxLength="560"
             cols="45"
             wrap="hard"
             onChange={handleSubmit}
           />
-  
+
           <div className="reviewComposeButton">
-            <button
-              className="reviewButton"
-              type="submit"
-            >
+            <button className="reviewButton" type="submit">
               Submit
             </button>
           </div>
@@ -58,3 +68,18 @@ const ReviewCompose = (props) => {
 };
 
 export default ReviewCompose;
+
+// const [user, setUser] = useState([]);
+
+// trying to grab the username using body.username?
+//   useEffect(() => {
+//   async function getUser(){
+//     const url = "http://localhost:5000/review";
+//     await axios({url:url, mode:"cors"}).then((response) => {
+//       setUser(response.data);
+//       localStorage.setItem("username", "ben");
+//       console.log(localStorage.getItem("username"));
+//     })
+//   }
+//     getUser();
+// }, [])
